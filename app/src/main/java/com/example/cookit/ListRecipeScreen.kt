7c05d86d69c71f1b9163724e2b9754cf76a74aff
mplanasm <maria.yujie.planas@estudiantat.upc.edu>
2024-12-04 11:16:26 +0100

    no dependency problems

--- app/src/main/java/com/example/cookit/ListRecipeScreen.kt
+++ app/src/main/java/com/example/cookit/ListRecipeScreen.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
