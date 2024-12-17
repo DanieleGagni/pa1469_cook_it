e8d7f9d6b9f1fe5411ccfe27debd091b5e6fa14d
Atsuko-Otsuka <o2210116@gl.cc.uec.ac.jp>
2024-12-18 00:09:29 +0900

    1218RecipeCreate

--- app/src/main/java/com/example/cookit/screens/createRecipe/CreateRecipeScreen.kt
+++ app/src/main/java/com/example/cookit/screens/createRecipe/CreateRecipeScreen.kt
@@ -8,13 +8,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
@@ -34,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -45,12 +49,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import kotlin.math.truncate
 
 @Composable
 fun AddEditIngredients( ingredients: MutableList<String>) {
     var currentIngredient by remember { mutableStateOf("") }
     var editIndex by remember { mutableStateOf(-1) }
     //val ingredients = remember { mutableStateListOf<String>() }
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Column (
         horizontalAlignment = Alignment.CenterHorizontally
@@ -66,30 +72,23 @@ fun AddEditIngredients( ingredients: MutableList<String>) {
                     text = if (editIndex >= 0) "Edit Ingredient" else "Enter Ingredient"
                 )
             },
-            maxLines = 1
-        )
-        Button(
-            onClick = {
-                if (currentIngredient.isNotBlank()) {
-                    if (editIndex >= 0) {
-                        ingredients[editIndex] = currentIngredient
-                        editIndex = -1
-                    } else {
-                        ingredients.add(currentIngredient)
+            maxLines = 1,
+            singleLine = true,
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    if (currentIngredient.isNotBlank()) {
+                        if (editIndex >= 0) {
+                            ingredients[editIndex] = currentIngredient
+                            editIndex = -1
+                        } else {
+                            ingredients.add(currentIngredient)
+                        }
+                        currentIngredient = ""
+                        keyboardController?.hide()
                     }
-                    currentIngredient = ""
                 }
-            },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xB3F58D1E),
-                contentColor = Color.Black
-            ),
-            modifier = Modifier.padding(8.dp)
-        ) {
-            Text(
-                text = if (editIndex >= 0) "Update Ingredient" else "Add Ingredient"
             )
-        }
+        )
         Column(
             Modifier
                 .fillMaxWidth()
@@ -158,6 +157,8 @@ fun AddEditStepsScreen(steps: MutableList<String>) {
     var editIndex by remember { mutableStateOf(-1) }
     var insertIndex by remember { mutableStateOf(-1) }
     //val steps = remember { mutableStateListOf<String>() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Column (
         horizontalAlignment = Alignment.CenterHorizontally
     ){
@@ -176,39 +177,30 @@ fun AddEditStepsScreen(steps: MutableList<String>) {
                     }
                 )
             },
-            maxLines = 1
-        )
-        Button(
-            onClick = {
-                if (currentStep.isNotBlank()) {
-                    when {
-                        editIndex >= 0 -> {
-                            steps[editIndex] = currentStep
-                            editIndex = -1
+            maxLines = 1,
+            singleLine = true,
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    if (currentStep.isNotBlank()) {
+                        when {
+                            editIndex >= 0 -> {
+                                steps[editIndex] = currentStep
+                                editIndex = -1
+                            }
+                            insertIndex >= 0 -> {
+                                steps.add(insertIndex, currentStep)
+                                insertIndex = -1
+                            }
+                            else -> {
+                                steps.add(currentStep)
+                            }
                         }
-
-                        insertIndex >= 0 -> {
-                            steps.add(insertIndex, currentStep)
-                            insertIndex = -1
-                        }
-
-                        else -> {
-                            steps.add(currentStep)
-                        }
+                        currentStep = ""
+                        keyboardController?.hide()
                     }
-                    currentStep = ""
                 }
-            },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xB3F58D1E),
-                contentColor = Color.Black
-            ),
-            modifier = Modifier.padding(8.dp)
-        ) {
-            Text(
-                text = if (editIndex >= 0) "Update Step" else "Add Step"
             )
-        }
+        )
         Column(
             Modifier
                 .fillMaxWidth()
@@ -302,8 +294,13 @@ fun CreateRecipeScreen(
     val ingredients = remember { mutableStateListOf<String>() }
     val steps = remember { mutableStateListOf<String>() }
 
+    val keyboardController = LocalSoftwareKeyboardController.current
+
 
     LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .imePadding(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         item {
@@ -351,6 +348,12 @@ fun CreateRecipeScreen(
                     onValueChange = { title = it },
                     label = { Text(text = "Enter Menu Name") },
                     maxLines = 1,
+                    singleLine = true,
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            keyboardController?.hide()
+                        }
+                    )
                 )
             }
         }
@@ -372,7 +375,13 @@ fun CreateRecipeScreen(
                     value = type,
                     onValueChange = { type = it },
                     label = { Text(text = "Enter Type") },
-                    maxLines = 1
+                    maxLines = 1,
+                    singleLine = true,
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            keyboardController?.hide()
+                        }
+                    )
                 )
             }
         }
@@ -394,7 +403,13 @@ fun CreateRecipeScreen(
                     value = estimatedTime,
                     onValueChange = { estimatedTime = it },
                     label = { Text(text = "Enter Estimated Time (minutes)") },
-                    maxLines = 1
+                    maxLines = 1,
+                    singleLine = true,
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            keyboardController?.hide()
+                        }
+                    )
                 )
             }
         }
@@ -416,7 +431,13 @@ fun CreateRecipeScreen(
                     value = serves,
                     onValueChange = { serves = it },
                     label = { Text(text = "Enter Servings") },
-                    maxLines = 1
+                    maxLines = 1,
+                    singleLine = true,
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            keyboardController?.hide()
+                        }
+                    )
                 )
             }
         }
