2fd0c2710b3e23b6a8084dabc8aa240ae013b821
daniele <daniele.gagni2000@gmail.com>
2024-12-17 17:09:44 +0100

    updated Recipe.kt, createRecipeScreen now adds recipe to Firebase

--- app/src/main/java/com/example/cookit/screens/createRecipe/CreateRecipeScreen.kt
+++ app/src/main/java/com/example/cookit/screens/createRecipe/CreateRecipeScreen.kt
@@ -47,9 +47,108 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.cookit.screens.components.Recipe
+import com.example.cookit.screens.logIn.LoginUiState
+import com.example.cookit.screens.logIn.LoginViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlin.math.truncate
+
+class CreateRecipeViewModel : ViewModel() {
+
+    private val db = Firebase.firestore
+    private val recipesCollection = db.collection("recipes")
+
+    fun addRecipe(recipe: Recipe) {
+
+        val updatedRecipe = recipe
+            .withTitleKeywords( extractTitleKeywords(recipe.title) )
+            .withIngredientsKeywords( extractIngredientsKeywords(recipe.ingredients) )
+
+        recipesCollection
+            .add(updatedRecipe)
+            .addOnSuccessListener { documentReference ->
+                Log.d("[------------------------- DEBUG]", "Recipe '${updatedRecipe.title}' added successfully.")
+                Log.d("[------------------------- DEBUG]", "Recipe added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.e("[------------------------- DEBUG]", "Error adding recipe '${updatedRecipe.title}': ${e.message}")
+            }
+    }
+
+    // extract keywords from recipe title
+    private fun extractTitleKeywords(title: String): List<String> {
+
+        // \b([A-Za-z]+)\b --> matches only alphabetic characters
+        val wordPattern = "\\b([A-Za-z]+)\\b".toRegex()
+
+        val STOP_WORDS = setOf(
+            "a", "an", "and", "as", "at", "by", "for", "from", "in", "into", "of",
+            "on", "or", "the", "to", "with", "your", "my", "our", "their", "this",
+            "that", "these", "those", "over", "under", "around", "up", "down", "out",
+            "inside", "outside", "through", "onto", "off"
+        )
+
+        val  extractedKeywords =  wordPattern.findAll(title)
+            .map { it.groupValues[1] } //extracts ONLY the captured word group
+            .map { it.lowercase() }
+            .filter { it !in STOP_WORDS }
+            .toList()
+
+        extractedKeywords.forEach { keyword ->
+            Log.d("[------------------------- DEBUG]", "------------------------- $keyword")
+        }
+
+        return extractedKeywords
+    }
+
+    // extract keywords from ingredients
+    private fun extractIngredientsKeywords(ingredients: List<String>):MutableList<String> {
+
+        val regex = """(?:\d+\s*[^a-zA-Z]*|\b)([a-zA-Z\s]+?)(?=\b\d*[^a-zA-Z]*$|\b)""".toRegex()
+
+        val STOP_WORDS = setOf(
+            "cup", "teaspoon", "tablespoon", "small", "medium", "large", "chopped", "diced", "minced", "fresh",
+            "optional", "to", "taste", "and", "or", "any", "half", "cooked", "ounces", "pounds", "g", "mg", "ml",
+            "liter", "kilogram", "gram", "liter", "tsp", "tbsp", "flour", "salt", "pepper", "oil", "water", "sugar",
+            "butter", "cheese", "egg"
+        )
+
+        val allKeywords = mutableListOf<String>()
+
+        ingredients.forEach { ingredient ->
+            val extractedKeywords = mutableListOf<String>()
+
+            regex.findAll(ingredient).forEach { matchResult ->
+
+                val keyword = matchResult.groupValues[1].trim().lowercase()
+
+                if (keyword.isNotBlank() && keyword !in STOP_WORDS) {
+                    allKeywords.add(keyword)
+                }
+            }
+        }
+
+        return allKeywords
+    }
+
+    fun Recipe.withTitleKeywords(titleKeywords: List<String>): Recipe {
+        return this.copy(title_keywords = titleKeywords)
+    }
+
+    fun Recipe.withIngredientsKeywords(ingredientsKeywords: List<String>): Recipe {
+        return this.copy(ingredients_keywords = ingredientsKeywords)
+    }
+
+
+
+}
 
 @Composable
 fun AddEditIngredients( ingredients: MutableList<String>) {
@@ -285,7 +384,8 @@ fun AddEditStepsScreen(steps: MutableList<String>) {
 
 @Composable
 fun CreateRecipeScreen(
-    navController: NavHostController // Pass the NavController for navigation
+    navController: NavHostController,
+    viewModel: CreateRecipeViewModel = viewModel()
 ) {
     var title by remember { mutableStateOf("") }
     var estimatedTime by remember { mutableStateOf("") }
@@ -502,21 +602,27 @@ fun CreateRecipeScreen(
         item {
             Button(
                 onClick = {
-                    // Add navigation to the home screen or perform your post action
-                    navController.navigate("home")
 
-                    Log.d("[------------------------- DEBUG]", "------------------------- $title")
-                    ingredients.forEach { ingredient ->
+                    val recipe = Recipe.create(
+                        title = title,
+                        ingredients = ingredients,
+                        estimatedTime = estimatedTime.toInt(),
+                        serves = serves.toInt(),
+                        steps = steps,
+                        type = type
+                    )
+
+
+                    Log.d("[------------------------- DEBUG]", "------------------------- ${recipe.title}")
+                    recipe.ingredients.forEach { ingredient ->
                         Log.d("[------------------------- DEBUG]", "------------------------- $ingredient")
                     }
                     steps.forEach { step ->
                         Log.d("[------------------------- DEBUG]", "------------------------- $step")
                     }
 
-
-                    // TODO create recipe
-                    // addRecipe(recipe)
-
+                    viewModel.addRecipe(recipe)
+                    navController.navigate("home")
 
                 },
                 colors = ButtonDefaults.buttonColors(
